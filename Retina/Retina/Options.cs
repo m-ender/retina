76fc88a91e1df088ef50781bf639012d0192f8cd
Martin Büttner <m.buettner.d@gmail.com>
2015-02-13 20:02:30 +0000

    Add regex options and grep modes

---
+++ Retina/Retina/Options.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Retina
+{
+    class Options
+    {
+        public RegexOptions RegexOptions { get; set; }
+
+        public Modes Mode { get; set; }
+
+        public Options()
+        {
+            RegexOptions = RegexOptions.None;
+            Mode = Modes.Match;
+        }
+    }
+}
--- Retina/Retina/Program.cs
+++ Retina/Retina/Program.cs
@@ -32,21 +32,39 @@ namespace Retina
             string input = instrm.ReadToEnd();
 
             string pattern = File.ReadAllText(patternFile);
-            char[] delimiter = {'\n'};
-            var lines = new List<string>(pattern.Split(delimiter));
+
+            var lines = new List<string>(pattern.Split(new [] {'`'}));
+
+            var options = new Options();
             if (lines.Count() > 1)
             {
-                string options = lines[0];
+                string optionString = lines[0];
                 lines.RemoveAt(0);
 
-                ParseOptions(options);
+                options = ParseOptions(optionString);
             }
 
-            Regex regex = new Regex(String.Join("\n", lines));
+            Regex regex = new Regex(String.Join("`", lines), options.RegexOptions);
 
-            MatchCollection matches = regex.Matches(input);
-
-            Console.WriteLine(matches.Count);
+            switch (options.Mode)
+            {
+            case Modes.Match:
+                MatchCollection matches = regex.Matches(input);
+                Console.WriteLine(matches.Count);
+                break;
+            case Modes.Grep:
+            case Modes.AntiGrep:
+                var stringReader = new StringReader(input);
+                string line;
+                while ((line = stringReader.ReadLine()) != null)
+                {
+                    if (regex.IsMatch(line) ^ (options.Mode == Modes.AntiGrep))
+                        Console.WriteLine(line);
+                }
+                break;
+            default:
+                throw new NotImplementedException();
+            }
         }
 
         private static void PerformReplacement(string patternFile, string replacementFile)
@@ -54,9 +72,58 @@ namespace Retina
             throw new NotImplementedException();
         }
 
-        private static void ParseOptions(string options)
+        private static Options ParseOptions(string optionString)
         {
-            throw new NotImplementedException();
+
+            var options = new Options();
+
+            foreach (char c in optionString)
+            {
+                switch (c)
+                {
+                // Parse RegexOptions
+                case 'c':
+                    options.RegexOptions |= RegexOptions.CultureInvariant;
+                    break;
+                case 'e':
+                    options.RegexOptions |= RegexOptions.ECMAScript;
+                    break;
+                case 'i':
+                    options.RegexOptions |= RegexOptions.IgnoreCase;
+                    break;
+                case 'm':
+                    options.RegexOptions |= RegexOptions.Multiline;
+                    break;
+                case 'n':
+                    options.RegexOptions |= RegexOptions.ExplicitCapture;
+                    break;
+                case 'r':
+                    options.RegexOptions |= RegexOptions.RightToLeft;
+                    break;                
+                case 's':
+                    options.RegexOptions |= RegexOptions.Singleline;
+                    break;
+                case 'x':
+                    options.RegexOptions |= RegexOptions.IgnorePatternWhitespace;
+                    break;
+
+                // Parse Mode
+                case 'G':
+                    options.Mode = Modes.Grep;
+                    break;
+                case 'A':
+                    options.Mode = Modes.AntiGrep;
+                    break;
+                case 'S':
+                    options.Mode = Modes.Split;
+                    break;
+
+                default:
+                    break;
+                }
+            }
+
+            return options;
         }
     }
 }
