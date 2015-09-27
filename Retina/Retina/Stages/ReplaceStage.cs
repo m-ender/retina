08f9334e6c881b4f1e5a8c3c9a9991861e2d699c
Martin Büttner <m.buettner.d@gmail.com>
2015-09-28 00:07:24 +0200

    Add basic framework for new Replace implementation

--- Retina/Retina/Stages/ReplaceStage.cs
+++ Retina/Retina/Stages/ReplaceStage.cs
@@ -4,21 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Retina.Replace;
 
 namespace Retina.Stages
 {
     public class ReplaceStage : RegexStage
     {
-        public string Replacement { get; set; }
+        public List<Token> Tokens { get; set; }
 
         public ReplaceStage(Options options, string pattern, string replacement) : base(options, pattern)
         {
-            Replacement = replacement;
+            ParseReplacement(replacement);
         }
 
+        private void ParseReplacement(string replacement)
+        {
+            Tokens = new List<Token>();
+
+            Tokens.Add(new Literal(replacement));
+        }
+            
         protected override StringBuilder Process(string input)
         {
-            return new StringBuilder(Pattern.Replace(input, Replacement));
+            var builder = new StringBuilder();
+
+            int i = 0;
+
+            foreach (Match m in Pattern.Matches(input))
+            {
+                builder.Append(input.Substring(i, m.Index-i));
+                foreach (Token t in Tokens)
+                    builder.Append(t.Process(m));
+                i = m.Index + m.Length;
+            }
+
+            builder.Append(input.Substring(i));
+
+            return builder;
         }
     }
 }
