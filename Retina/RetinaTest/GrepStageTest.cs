8e44a04e5c9fd1b2200550b1d1bcdbca964d41a3
Martin Ender <m.buettner.d@gmail.com>
2017-06-25 17:13:36 +0200

    Add testss for Grep and AntiGrep stages

---
+++ Retina/RetinaTest/GrepStageTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RetinaTest
+{
+    [TestClass]
+    public class GrepStageTest : RetinaTestBase
+    {
+        [TestMethod]
+        public void TestBasicGrep()
+        {
+            AssertProgram(new TestSuite { Sources = { @"G`" }, TestCases = { { "\nabc\n!!!\n\ndef\n", "\nabc\n!!!\n\ndef\n" } } });
+            AssertProgram(new TestSuite { Sources = { @"G`." }, TestCases = { { "\nabc\n!!!\n\ndef\n", "abc\n!!!\ndef" } } });
+            AssertProgram(new TestSuite { Sources = { @"G`\w" }, TestCases = { { "\nabc\n!!!\n\ndef\n", "abc\ndef" } } });
+        }
+
+        [TestMethod]
+        public void TestBasicAntiGrep()
+        {
+            AssertProgram(new TestSuite { Sources = { @"A`" }, TestCases = { { "\nabc\n!!!\n\ndef\n", "" } } });
+            AssertProgram(new TestSuite { Sources = { @"A`." }, TestCases = { { "\nabc\n!!!\n\ndef\n", "\n\n" } } });
+            AssertProgram(new TestSuite { Sources = { @"A`\w" }, TestCases = { { "\nabc\n!!!\n\ndef\n", "\n!!!\n\n" } } });
+        }
+    }
+}
