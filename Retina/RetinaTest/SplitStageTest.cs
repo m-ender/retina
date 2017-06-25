e4359f38ab550f350694c0a839341e492c653768
Martin Ender <m.buettner.d@gmail.com>
2017-06-25 15:24:48 +0200

    Add tests for Split stages

---
+++ Retina/RetinaTest/SplitStageTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RetinaTest
+{
+    [TestClass]
+    public class SplitStageTest : RetinaTestBase
+    {
+        [TestMethod]
+        public void TestBasicSplitting()
+        {
+            AssertProgram(new TestSuite { Sources = { @"S`\W+" }, TestCases = { { "Hello, World!", "Hello\nWorld\n" } } });
+            AssertProgram(new TestSuite { Sources = { @"S`\W*" }, TestCases = { { "Hello, World!", "\nH\ne\nl\nl\no\n\nW\no\nr\nl\nd\n\n" } } });
+            AssertProgram(new TestSuite { Sources = { @"S`\W" }, TestCases = { { "Hello, World!", "Hello\n\nWorld\n" } } });
+        }
+
+        [TestMethod]
+        public void TestCapturingGroups()
+        {
+            AssertProgram(new TestSuite { Sources = { @"S`(\W)+" }, TestCases = { { "Hello, World!", "Hello\n \nWorld\n!\n" } } });
+            AssertProgram(new TestSuite { Sources = { @"S`(.o)(.)" }, TestCases = { { "Hello, World!", "Hel\nlo\n,\n \nWo\nr\nld!" } } });
+            AssertProgram(new TestSuite { Sources = { @"S`(\b|l)" }, TestCases = { { "Hello, World!", "\n\nHe\nl\n\nl\no\n\n, \n\nWor\nl\nd\n\n!" } } });
+        }
+
+        [TestMethod]
+        public void TestWithoutCapturingGroups()
+        {
+            AssertProgram(new TestSuite { Sources = { @"S-`(\W)+" }, TestCases = { { "Hello, World!", "Hello\nWorld\n" } } });
+            AssertProgram(new TestSuite { Sources = { @"S-`(.o)(.)" }, TestCases = { { "Hello, World!", "Hel\n \nld!" } } });
+            AssertProgram(new TestSuite { Sources = { @"S-`(\b|l)" }, TestCases = { { "Hello, World!", "\nHe\n\no\n, \nWor\nd\n!" } } });
+        }
+
+        [TestMethod]
+        public void TestWithoutEmptySegments()
+        {
+            AssertProgram(new TestSuite { Sources = { @"S_`(\W)+" }, TestCases = { { "Hello, World!", "Hello\n \nWorld\n!" } } });
+            AssertProgram(new TestSuite { Sources = { @"S_`(.o)(.)" }, TestCases = { { "Hello, World!", "Hel\nlo\n,\n \nWo\nr\nld!" } } });
+            AssertProgram(new TestSuite { Sources = { @"S_`(\b|l)" }, TestCases = { { "Hello, World!", "\nHe\nl\nl\no\n\n, \n\nWor\nl\nd\n\n!" } } });
+        }
+    }
+}
