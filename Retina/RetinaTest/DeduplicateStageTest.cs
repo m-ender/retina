c093fd7aab2ecd6f091b62ffb70a25cbb36185aa
Martin Ender <m.buettner.d@gmail.com>
2017-06-25 18:10:23 +0200

    Add tests for Deduplicate stages

---
+++ Retina/RetinaTest/DeduplicateStageTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RetinaTest
+{
+    [TestClass]
+    public class DeduplicateStageTest : RetinaTestBase
+    {
+        [TestMethod]
+        public void TestBasicDeduplication()
+        {
+            AssertProgram(new TestSuite { Sources = { @"D`." }, TestCases = { { "abacbcedef", "abcedf" } } });
+            AssertProgram(new TestSuite { Sources = { @"D`\w+" }, TestCases = { { "abc def abc ab ghi def", "abc def  ab ghi " } } });
+        }
+
+        [TestMethod]
+        public void TestRTLMatching()
+        {
+            AssertProgram(new TestSuite { Sources = { @"Dr`." }, TestCases = { { "abacbcedef", "abcdef" } } });
+            AssertProgram(new TestSuite { Sources = { @"Dr`\w+" }, TestCases = { { "abc def abc ab ghi def", "  abc ab ghi def" } } });
+        }
+
+        [TestMethod]
+        public void TestDeduplicateBy()
+        {
+            AssertProgram(new TestSuite { Sources = { @"D$`(\w)\w*", "$1" }, TestCases = { { "abc def abc ab ghi def", "abc def   ghi " } } });
+        }
+    }
+}
