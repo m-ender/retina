e03908c4e5fa61dee1705fea055ec388826356c8
Martin Ender <m.buettner.d@gmail.com>
2017-06-27 21:40:24 +0200

    Add some basic tests for loops

---
+++ Retina/RetinaTest/LoopTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RetinaTest
+{
+    [TestClass]
+    public class LoopTest : RetinaTestBase
+    {
+        [TestMethod]
+        public void TestBasicLoop()
+        {
+            AssertProgram(new TestSuite { Sources = { @"+`." }, TestCases = { { "abcdefghijklmnopqrstuvwxyz", "1" } } });
+            AssertProgram(new TestSuite { Sources = { @"+`(.)\1", @"$1" }, TestCases = { { "aaaaaaabbbbbbcccccccddddeeeee", "abcde" } } });
+        }
+
+        [TestMethod]
+        public void TestGroupLoop()
+        {
+            AssertProgram(new TestSuite {
+                Sources = {
+                    @"{`ab",
+                    @"c",
+                    @"}`ac",
+                    @"b"
+                },
+                TestCases = { { "aaaaaaaab", "b" } }
+            });
+        }
+    }
+}
