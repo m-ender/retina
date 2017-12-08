46277c9af896684a1dc9ca6d782808f26cd8556c
Martin Ender <m.buettner.d@gmail.com>
2017-12-08 22:58:51 +0100

    And unit tests for output stages...

---
+++ Retina/RetinaTest/OutputStageTest.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RetinaTest
+{
+    [TestClass]
+    public class OutputStageTest : RetinaTestBase
+    {
+        [TestMethod]
+        public void TestDefaultOutput()
+        {
+            AssertProgram(new TestSuite
+            {
+                Sources = { "G`", "G`" },
+                TestCases = { { "Hello, World!", "Hello, World!" } }
+            });
+        }
+
+        [TestMethod]
+        public void TestSilentFlag()
+        {
+            // It shouldn't matter where the . flag is used.
+            AssertProgram(new TestSuite
+            {
+                Sources = { ".G`", "G`" },
+                TestCases = { { "Hello, World!", "" } }
+            });
+            AssertProgram(new TestSuite
+            {
+                Sources = { "G`", ".G`" },
+                TestCases = { { "Hello, World!", "" } }
+            });
+        }
+        
+        [TestMethod]
+        public void TestExplicitOutput()
+        {
+            // : on the last stage should be swallowed by implicit print.
+            AssertProgram(new TestSuite
+            {
+                Sources = { "G`", ":G`" },
+                TestCases = { { "Hello, World!", "Hello, World!" } }
+            });
+
+            // : anywhere else should print the intermediate result of that stage
+            AssertProgram(new TestSuite
+            {
+                Sources = { ":O`.", "G`" },
+                TestCases = { { "Hello, World!", " !,HWdellloor !,HWdellloor" } }
+            });
+        }
+
+        [TestMethod]
+        public void TestPrintOnlyIfChanged()
+        {
+            // ; on the last stage should replace the implicit :
+            AssertProgram(new TestSuite
+            {
+                Sources = { "G`", ";G`" },
+                TestCases = { { "Hello, World!", "" } }
+            });
+            AssertProgram(new TestSuite
+            {
+                Sources = { "G`", ";O`." },
+                TestCases = { { "Hello, World!", " !,HWdellloor" } }
+            });
+
+
+            AssertProgram(new TestSuite
+            {
+                Sources = { ";G`", "G`" },
+                TestCases = { { "Hello, World!", "Hello, World!" } }
+            });
+            AssertProgram(new TestSuite
+            {
+                Sources = { ";O`.", "G`" },
+                TestCases = { { "Hello, World!", " !,HWdellloor !,HWdellloor" } }
+            });
+        }
+
+        [TestMethod]
+        public void TestTrailingLinefeed()
+        {
+            // :\
+            AssertProgram(new TestSuite
+            {
+                Sources = { @"G`", @":\G`" },
+                TestCases = { { "Hello, World!", "Hello, World!\n" } }
+            });
+            AssertProgram(new TestSuite
+            {
+                Sources = { @"G`", @"\G`" },
+                TestCases = { { "Hello, World!", "Hello, World!\n" } }
+            });
+
+
+            // \ as a shorthand for :\
+            AssertProgram(new TestSuite
+            {
+                Sources = { @":\G`", @"G`" },
+                TestCases = { { "Hello, World!", "Hello, World!\nHello, World!" } }
+            });
+            AssertProgram(new TestSuite
+            {
+                Sources = { @"\G`", @"G`" },
+                TestCases = { { "Hello, World!", "Hello, World!\nHello, World!" } }
+            });
+
+
+            // ;\
+            AssertProgram(new TestSuite
+            {
+                Sources = { @"G`", @";\G`" },
+                TestCases = { { "Hello, World!", "" } }
+            });
+            AssertProgram(new TestSuite
+            {
+                Sources = { @"G`", @";\O`." },
+                TestCases = { { "Hello, World!", " !,HWdellloor\n" } }
+            });
+
+            AssertProgram(new TestSuite
+            {
+                Sources = { @";\G`", @"G`" },
+                TestCases = { { "Hello, World!", "Hello, World!" } }
+            });
+            AssertProgram(new TestSuite
+            {
+                Sources = { @";\O`.", @"G`" },
+                TestCases = { { "Hello, World!", " !,HWdellloor\n !,HWdellloor" } }
+            });
+        }
+    }
+}
