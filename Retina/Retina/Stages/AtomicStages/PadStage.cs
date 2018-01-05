26c5a61f677403379785bd3ddc316fdd9bbcf719
Martin Ender <m.buettner.d@gmail.com>
2018-01-05 17:54:19 +0100

    Actually add them...

---
+++ Retina/Retina/Stages/AtomicStages/PadStage.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using Retina.Configuration;
+using Retina.Extensions;
+using System;
+
+namespace Retina.Stages
+{
+    public class PadStage : AtomicStage
+    {
+        public PadStage(Config config, List<string> patterns, List<string> substitutions, string separatorSubstitutionSource)
+            : base(config, patterns, substitutions, separatorSubstitutionSource) { }
+
+        protected override string Process(string input, TextWriter output)
+        {
+            // TODO:
+            // - Random option
+            
+            var separators = Separators.Select(s => s.Match.Value);
+
+            int paddingWidth = Matches.Select(m => m.Replacement.Length).Aggregate(Math.Max);
+
+            string padString;
+            if (!Config.Reverse)
+                padString = new string(Enumerable.Range(0, paddingWidth).Select(
+                    i => Config.PadString[i % Config.PadString.Length]
+                ).ToArray());
+            else
+                padString = new string(Enumerable.Range(0, paddingWidth).Reverse().Select(
+                    i => Config.PadString[Config.PadString.Length - 1 - i % Config.PadString.Length]
+                ).ToArray());
+
+            var paddedMatches = Matches.Select(m =>
+            {
+                string val = m.Match.Value;
+                if (!Config.Reverse)
+                    return val + padString.Remove(0, Math.Min(val.Length, padString.Length));
+                else
+                    return padString.Substring(0, Math.Max(0, padString.Length - val.Length)) + val;
+            });
+            
+            return separators.Riffle(paddedMatches);
+        }
+    }
+}
---
+++ Retina/RetinaTest/PadStageTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Retina;
+using Retina.Stages;
+
+namespace RetinaTest
+{
+    [TestClass]
+    public class PadStageTest : RetinaTestBase
+    {
+        [TestMethod]
+        public void TestBasicPadding()
+        {
+            AssertProgram(new TestSuite { Sources = { "P`.+" }, TestCases = { { "ABC\nAB\nab\nabcd\nxyz\nXYZ", "ABC \nAB  \nab  \nabcd\nxyz \nXYZ " } } });
+        }
+
+        [TestMethod]
+        public void TestLeftPadding()
+        {
+            AssertProgram(new TestSuite { Sources = { "P^`.+" }, TestCases = { { "ABC\nAB\nab\nabcd\nxyz\nXYZ", " ABC\n  AB\n  ab\nabcd\n xyz\n XYZ" } } });
+        }
+
+        [TestMethod]
+        public void TestPadBy()
+        {
+            // Replacements are used to determine the padding width
+            AssertProgram(new TestSuite { Sources = { @"P$`.", "$*" }, TestCases = { { "1234", "1   2   3   4   " } } });
+            // That also means the padding width can be shorter than some matches
+            AssertProgram(new TestSuite { Sources = { @"P$`.+", "abc" }, TestCases = { { "ABC\nAB\na\nabcd\nxyz\nXYZ", "ABC\nAB \na  \nabcd\nxyz\nXYZ" } } });
+        }
+
+        [TestMethod]
+        public void TestPaddingCharacter()
+        {
+            AssertProgram(new TestSuite { Sources = { "P'_`.+" }, TestCases = { { "ABC\nAB\nab\nabcd\nxyz\nXYZ", "ABC_\nAB__\nab__\nabcd\nxyz_\nXYZ_" } } });
+        }
+
+        [TestMethod]
+        public void TestPaddingString()
+        {
+            // The string gets repeated cyclically from the left (or from the right for left padding).
+            AssertProgram(new TestSuite { Sources = { @"P"".oOo""`.+" }, TestCases = { { "a\nbc\ndef\n0123456789", "aoOo.oOo.o\nbcOo.oOo.o\ndefo.oOo.o\n0123456789" } } });
+            AssertProgram(new TestSuite { Sources = { @"P"".oOo""^`.+" }, TestCases = { { "a\nbc\ndef\n0123456789", "Oo.oOo.oOa\nOo.oOo.obc\nOo.oOo.def\n0123456789" } } });
+        }
+
+        [TestMethod]
+        public void TestOverlappingMatches()
+        {
+            AssertProgram(new TestSuite { Sources = { @"Pv`.+" }, TestCases = { { "abcd", "abcdbcd cd  d   " } } });
+            AssertProgram(new TestSuite { Sources = { @"Pw`.+" }, TestCases = { { "abcd", "a   ab  abc abcdb   bc  bcd c   cd  d   " } } });
+        }
+    }
+}
