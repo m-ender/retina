4f895f6fc2bc9bdb10e626cd399027b8b14f0ec5
Martin Ender <m.buettner.d@gmail.com>
2018-01-13 00:52:52 +0100

    Add another test

--- Retina/RetinaTest/EvalStageTest.cs
+++ Retina/RetinaTest/EvalStageTest.cs
@@ -22,6 +22,20 @@ namespace RetinaTest
                 },
                 TestCases = { { "", "12\n12612abcabc\n848114cabc\n636103abc\n42492bc\n00070\nabc,abcabc,,00070,,,," } }
             });
+
+
+            AssertProgram(new TestSuite
+            {
+                Sources =
+                {
+                    @"^",
+                    @"abc",
+                    @"~`.+",
+                    @".+$n$$&$$&$n{$*`.+$n$$&$$&$n\`^$n$$.-0$$.-1$$.+2$$.+3$$.+4$n)`^.{0,7}$n$n^$$$n$$+0,$$+1,$$+2,$$+3,$$+4,$$+5,$$+6,$$+7",
+                    @"*\L`^..",
+                },
+                TestCases = { { "", "12\n12612abcabc\n848114cabc\n636103abc\n42492bc\n00070\nabc,abcabc,,00070,,,," } }
+            });
         }
     }
 }
