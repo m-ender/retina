m-ender/retina

commit 444e8168b4aae229cab7c517802919e768c73080

Use BigIntegers for lazy length computation

Retina/Retina/Replace/Nodes/Concatenation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
 namespace Retina.Replace.Nodes
 {
@@ -18,9 +19,9 @@ namespace Retina.Replace.Nodes
             return String.Concat(Children.Select(c => c.GetString(input, matches, separators, index)));
         }
 
-        public override int GetLength(string input, List<MatchContext> matches, List<MatchContext> separators, int index)
+        public override BigInteger GetLength(string input, List<MatchContext> matches, List<MatchContext> separators, int index)
         {
-            return Children.Select(c => c.GetLength(input, matches, separators, index)).Sum();
+            return Children.Select(c => c.GetLength(input, matches, separators, index)).Aggregate((cur, sum) => sum + cur);
         }
     }
 }

Retina/Retina/Replace/Nodes/DynamicElement.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text.RegularExpressions;
 
 namespace Retina.Replace.Nodes
@@ -31,7 +32,7 @@ namespace Retina.Replace.Nodes
             return ResolveChild(input, matches, separators, index);
         }
 
-        public override int GetLength(string input, List<MatchContext> matches, List<MatchContext> separators, int index)
+        public override BigInteger GetLength(string input, List<MatchContext> matches, List<MatchContext> separators, int index)
         {
             return ResolveChild(input, matches, separators, index).Length;
         }

Retina/Retina/Replace/Nodes/Length.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace Retina.Replace.Nodes
 {
@@ -17,7 +18,7 @@ namespace Retina.Replace.Nodes
             return Child.GetLength(input, matches, separators, index).ToString();
         }
 
-        public override int GetLength(string input, List<MatchContext> matches, List<MatchContext> separators, int index)
+        public override BigInteger GetLength(string input, List<MatchContext> matches, List<MatchContext> separators, int index)
         {
             return Child.GetLength(input, matches, separators, index).ToString().Length;
         }

Retina/Retina/Replace/Nodes/Literal.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -21,7 +22,7 @@ namespace Retina.Replace.Nodes
             return Value;
         }
 
-        public override int GetLength(string input, List<MatchContext> matches, List<MatchContext> separators, int index)
+        public override BigInteger GetLength(string input, List<MatchContext> matches, List<MatchContext> separators, int index)
         {
             return Value.Length;
         }

Retina/Retina/Replace/Nodes/LowerCaseAll.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Numerics;
 
 namespace Retina.Replace.Nodes
 {
@@ -16,7 +17,7 @@ namespace Retina.Replace.Nodes
             return Child.GetString(input, matches, separators, index).ToLowerInvariant();
         }
 
-        public override int GetLength(string input, List<MatchContext> matches, List<MatchContext> separators, int index)
+        public override BigInteger GetLength(string input, List<MatchContext> matches, List<MatchContext> separators, int index)
         {
             return Child.GetLength(input, matches, separators, index);
         }

Retina/Retina/Replace/Nodes/LowerCaseHead.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace Retina.Replace.Nodes
 {
@@ -22,7 +23,7 @@ namespace Retina.Replace.Nodes
                 return Char.ToLowerInvariant(value[0]) + value.Substring(1);
         }
 
-        public override int GetLength(string input, List<MatchContext> matches, List<MatchContext> separators, int index)
+        public override BigInteger GetLength(string input, List<MatchContext> matches, List<MatchContext> separators, int index)
         {
             return Child.GetLength(input, matches, separators, index);
         }

Retina/Retina/Replace/Nodes/Node.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Numerics;
 
 namespace Retina.Replace.Nodes
 {
     public abstract class Node
     {
         abstract public string GetString(string input, List<MatchContext> matches, List<MatchContext> separators, int index);
-        abstract public int GetLength(string input, List<MatchContext> matches, List<MatchContext> separators, int index);
+        abstract public BigInteger GetLength(string input, List<MatchContext> matches, List<MatchContext> separators, int index);
     }
 }

commit 7ee44fc7c074033c5674cd8efe32867fda5d3322

Provide addititve identity when taking the sum
Commit 444e816 introduced a bug whereby `$.()` causes the program to crash because it tries to take the sum of a zero-length list without providing the additive identity.

Retina/Retina/Replace/Nodes/Concatenation.cs
@@ -21,7 +21,7 @@ namespace Retina.Replace.Nodes
 
         public override BigInteger GetLength(string input, List<MatchContext> matches, List<MatchContext> separators, int index)
         {
-            return Children.Select(c => c.GetLength(input, matches, separators, index)).Aggregate((cur, sum) => sum + cur);
+            return Children.Select(c => c.GetLength(input, matches, separators, index)).Aggregate(BigInteger.Zero, (cur, sum) => sum + cur);
         }
     }
 }